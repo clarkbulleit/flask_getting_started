4d2ee17a057331938b51414e5688764d1085c6a5
Clark Bulleit <clark.bulleit@duke.edu>
2018-11-08 19:34:21 -0500

    added distance formula

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import math
 app = Flask(__name__)
 
 
@@ -15,10 +16,16 @@ def hello(name):
 
     return jsonify(d)
 
+
 @app.route("/distance", methods=["POST"])
 def distance():
     r = request.get_json()
-    
+    dist = math.sqrt((r["a"][0] - r["b"][0]) ^ 2 + ((r["a"][1] - r["b"][1]) ^ 2))
+    r["distance"] = round(dist, 2)
+
+    return jsonify(r), 200
+
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5001)
+    app.run(host="0.0.0.0", port=5000)
+    # app.run(host="127.0.0.1", port=5001)
